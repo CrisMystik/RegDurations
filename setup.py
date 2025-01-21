f83860b71ba1a70bc3eccc6d869a591bf58f3515
CrisMystik <62919067+CrisMystik@users.noreply.github.com>
2025-01-22 00:36:29 +0100

    Change version(s)

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name='regdurations',
     packages=find_packages(include=['regdurations']),
-    version='1.0.1',
-    python_requires='>=3.4',
+    version='1.0.2',
+    python_requires='>=3.9',
     extras_require={
         'find_relativedelta': ['dateutil']
     },
