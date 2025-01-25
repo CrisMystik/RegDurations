111ddff6eb2fd05a8ee3e28153d6b09030579979
CrisMystik <62919067+CrisMystik@users.noreply.github.com>
2025-01-25 20:34:02 +0100

    FIX: re-order checks

--- regdurations/regdurations.py
+++ regdurations/regdurations.py
@@ -41,14 +41,16 @@ class DurationParser:
         for match in matches:
             if (not match) or (not match.group().strip()):
                 break
+
+            if (last is not None) and (match.start() != last):
+                break
+            last = match.end()
+
             if match.group(3):
                 continue
 
             if first is None:
                 first = match.start()
-            if (last is not None) and (match.start() != last):
-                break
-            last = match.end()
 
             match_type = next(
                 (k for k, v in self.allowed_patterns.items() if match.group(2).lower() in v), None
@@ -58,7 +60,7 @@ class DurationParser:
 
             result[match_type] += int(match.group(1))
 
-        return result, first, last
+        return result, first, (last if first is not None else None)
 
     def find_relativedelta(self, text: str) -> tuple['relativedelta', Union[int, None], Union[int, None]]:
         raise ModuleNotFoundError('dateutil module not available')
